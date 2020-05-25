edd363eba55bb392732509cf57a6f39a618ea9a4
DoSSi4 <57913839+DoSSi4@users.noreply.github.com>
2020-05-25 16:55:05 +0600

    Add files via upload

---
+++ src/dossi4/Builder.java
@@ -0,0 +1,11 @@
+package dossi4;
+
+public interface Builder {
+    void setType(Type type);
+    void windows(int windows);
+    void rooms(int rooms);
+    void doors(int doors);
+    void isHasGarage(boolean hasGarage);
+    void isHasGarden(boolean hasGarden);
+    void isHasSwimPool(boolean hasSwimPool);
+}
---
+++ src/dossi4/Type.java
@@ -0,0 +1,5 @@
+package dossi4;
+
+public enum  Type {
+    WITH_POOL,WITH_GARDEN,WITH_GARAGE
+}
